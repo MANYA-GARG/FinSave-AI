73a9714ab79b4f132fc464dc98c46a4b3bb838e3
MANYA-GARG <manyagarg754@gmail.com>
2026-01-18 00:49:14 +0530

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -23,7 +23,7 @@ const app=express();
 //middlewares
 
 const corsOptions={
-    origin:"http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
 
 }
@@ -31,6 +31,7 @@ app.use(cors(corsOptions));
 
 app.use(express.json({limit: "100mb"}));
 app.use(express.urlencoded({limit:"100mb",extended :true}));
+app.set("trust proxy", 1);
 app.use(
     session({
     secret: process.env.SESSION_SECRET || "secret",
