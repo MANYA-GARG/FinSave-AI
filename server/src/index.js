7bf908873904ad76457e1ca3ece5e0bafdfd1827
MANYA-GARG <manyagarg754@gmail.com>
2026-01-18 02:31:51 +0530

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -21,13 +21,15 @@ dbConnect();
 const app=express();
 
 //middlewares
-
-const corsOptions={
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+app.use(
+cors({
+    origin: process.env.FRONTEND_URL,
     credentials: true,
-
-}
-app.use(cors(corsOptions));
+     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+})
+);
+app.options("*", cors());
 
 app.use(express.json({limit: "100mb"}));
 app.use(express.urlencoded({limit:"100mb",extended :true}));
@@ -38,6 +40,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie:{
+        httpOnly: true,
         maxAge:60000 * 60,
         // Only send cookies over HTTPS in production
             secure: process.env.NODE_ENV === "production", 
