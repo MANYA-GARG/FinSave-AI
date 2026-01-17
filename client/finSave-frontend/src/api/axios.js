b4de2337e957c31e8f7c404824a74b2548f34a40
MANYA-GARG <manyagarg754@gmail.com>
2026-01-18 02:15:10 +0530

    Update axios.js

--- client/finSave-frontend/src/api/axios.js
+++ client/finSave-frontend/src/api/axios.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+console.log("DEBUG: API Base URL is ->", import.meta.env.VITE_API_BASE_URL);
+
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:7001/api", // your backend
   withCredentials: true,
