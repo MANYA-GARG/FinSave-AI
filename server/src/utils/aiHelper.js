dc9226b8594a6c39ae225ada205258abfa89c864
MANYA-GARG <manyagarg754@gmail.com>
2026-01-18 12:12:23 +0530

    Update aiHelper.js

--- server/src/utils/aiHelper.js
+++ server/src/utils/aiHelper.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 import Category from "../models/Category.js";
 
-const GEMINI_API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent";
+const GEMINI_API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent";
 
 const API_KEY = process.env.GEMINI_API_KEY;
 
